f9d67c8f5fe8ffdb7f1550bcd1a924d1741d0144
mitja <mitjafilej431@gmail.com>
2025-07-03 07:31:03 +0200

    simple reading of data and recognizing it

--- Extract.java
+++ Extract.java
@@ -1,8 +1,34 @@
 import java.util.*;
+import java.io.*;
 
 public class Extract{
     public static void main(String[] args) {
-        
+        System.out.println("Reading CSV using BufferedReader");
+
+        String path = "sample.csv";
+
+        List<List<String>> data = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            /* simple reading of data and recognizing it */
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                List<String> lineData = Arrays.asList(values);
+
+                data.add(lineData);
+            }
+
+            System.out.println("\nData read from CSV file:");
+            for (int i = 0; i < data.size(); i++) {
+                List<String> row = data.get(i);
+                System.out.println("Row " + i + ": " + String.join(", ", row));
+            }
+
+        } catch (Exception e) {
+            
+        }
     }
     
 }
--- Metadata.java
+++ Metadata.java
@@ -38,11 +38,212 @@ public class Metadata{
         this.genres = genres;
         this.recordLabel= recordLabel;
         this.danceability = danceability;
-        
+        this.energy = energy;
+        this.key = key;
+        this.loudness = loudness;
+        this.mode = mode;
+        this.speechiness = speechiness;
+        this.acousticness = acousticness;
+        this.instrumentalness = instrumentalness;
+        this.liveness = liveness;
+        this.valance = valance;
+        this.tempo = tempo;
+        this.timeSignature = timeSignature;
     }
 
     public Metadata(){
 
     }
 
+    public String getTrackURI() {
+        return trackURI;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public String getArtistNames() {
+        return artistNames;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public boolean getExplicit(){
+        return explicit;
+    }
+
+    public String getAddedBy() {
+        return addedBy;
+    }
+
+    public String getAddedAt() {
+        return addedAt;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+
+    public double getDanceability() {
+        return danceability;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public double getLoudness() {
+        return loudness;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public double getSpeechiness() {
+        return speechiness;
+    }
+
+    public double getAcousticness() {
+        return acousticness;
+    }
+
+    public double getInstrumentalness() {
+        return instrumentalness;
+    }
+
+    public double getLiveness() {
+        return liveness;
+    }
+
+    public double getValance() {
+        return valance;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+
+    public int getTimeSignature() {
+        return timeSignature;
+    }
+
+    public void setTrackURI(String trackURI) {
+        this.trackURI = trackURI;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public void setArtistNames(String artistNames) {
+        this.artistNames = artistNames;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public void setExplicit(boolean explicit) {
+        this.explicit = explicit;
+    }
+
+    public void setAddedBy(String addedBy) {
+        this.addedBy = addedBy;
+    }
+
+    public void setAddedAt(String addedAt) {
+        this.addedAt = addedAt;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public void setRecordLabel(String recordLabel) {
+        this.recordLabel = recordLabel;
+    }
+
+    public void setDanceability(double danceability) {
+        this.danceability = danceability;
+    }
+    
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setLoudness(double loudness) {
+        this.loudness = loudness;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public void setSpeechiness(double speechiness) {
+        this.speechiness = speechiness;
+    }
+
+    public void setAcousticness(double acousticness) {
+        this.acousticness = acousticness;
+    }
+
+    public void setInstrumentalness(double instrumentalness) {
+        this.instrumentalness = instrumentalness;
+    }
+
+    public void setLiveness(double liveness) {
+        this.liveness = liveness;
+    }
+
+    public void setValance(double valance) {
+        this.valance = valance;
+    }
+
+    public void setTempo(double tempo) {
+        this.tempo = tempo;
+    }
+
+    public void setTimeSignature(int timeSignature) {
+        this.timeSignature = timeSignature;
+    }
 }
