7b32920a9087e6c9962abc064c20a70fce4d03c4
mitja <mitjafilej431@gmail.com>
2025-07-03 08:43:26 +0200

    started on breaking down data in single lines

--- Extract.java
+++ Extract.java
@@ -8,15 +8,50 @@ public class Extract{
         String path = "sample.csv";
 
         List<List<String>> data = new ArrayList<>();
+        String name = "line";
+        Metadata line1 = new Metadata();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             String line;
+            String linetemp = "";
+            int number;
             /* simple reading of data and recognizing it */
+            line = br.readLine(); //to avoid the first line which is not data
+            line = br.readLine();
+            line = br.readLine();
+            line = br.readLine();
+            line = br.readLine();
+            line = br.readLine();
+            
+
+            while (line != null) {
+            number = line.indexOf(",");
+            int num2 = line.indexOf("\"");
+            
+            //check for multiple artist, genres...
+            if(num2 != -1){
+                linetemp = line.substring(0, number);
+                System.out.println("D");
+            } else{
+
+            }
+            linetemp = line.substring(0, number);
+            line = line.substring(number+1, line.length());
+            System.out.println(linetemp);
+            }
+
+            //next put data in object
+
+
+
+/* 
+reference
+
             while((line = br.readLine()) != null){
+                System.out.print(line.indexOf(","));
                 String[] values = line.split(",");
                 List<String> lineData = Arrays.asList(values);
-
                 data.add(lineData);
             }
 
@@ -25,6 +60,9 @@ public class Extract{
                 List<String> row = data.get(i);
                 System.out.println("Row " + i + ": " + String.join(", ", row));
             }
+*/
+
+
 
         } catch (Exception e) {
             
